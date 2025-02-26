306c53a88f0c6cdba45dfb8b2b76134cb7a540c8
ezqHerrera <rodherrera14@gmail.com>
2025-02-26 17:37:41 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div id="main">
+      <div id='preguntaContainer'>
+        <h1></h1>
+      </div>
+
+      <div id='botonesContainer'>
+        <button type='button' className='respBtn btn btn-light'></button>
+        <button type='button' className='respBtn btn btn-light'></button>
+        <button type='button' className='respBtn btn btn-light'></button>
+        <button type='button' className='respBtn btn btn-light'></button>
+      </div>
+
+      <div id='infoBtnContainer' className='btn-group'>
+        <button disabled className='infoBtn'></button>
+        <button disabled className='infoBtn'></button>
+      </div>
+    </div>
+  )
+}
+
+export default App
